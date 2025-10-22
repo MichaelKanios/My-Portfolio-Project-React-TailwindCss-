280d3c37ec3ae5692a0641240158d79dac094d7c
Michael Kanios <mkanios77@gmail.com>
2025-10-22 08:43:28 +0000

    Finalize

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import emailjs from "emailjs-com";
+
 
 const schema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters"),
